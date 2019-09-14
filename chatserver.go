1d2743255053194c7cc03cc3983315e2b4b38604
Zach <zgiles@gmail.com>
2019-09-14 13:55:30 -0400

    Convert to kingpin cli and split files

---
+++ chatserver.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/gorilla/websocket"
+	"github.com/nats-io/gnatsd/server"
+	"github.com/nats-io/go-nats"
+)
+
+type ChatServer struct {
+	clients       map[*websocket.Conn]bool
+	broadcast     chan Message
+	upgrader      websocket.Upgrader
+	natsconnected bool
+	nc            *nats.Conn
+	ns            *server.Server
+	writemu       sync.Mutex
+}
+
+type Message struct {
+	Name     string `json:"name"`
+	Message  string `json:"message"`
+	Pong     bool   `json:"pong"`
+	Presence bool   `json:"presence"`
+	Id       string `json:"id"`
+}
+
+func (cs *ChatServer) handleChat(w http.ResponseWriter, r *http.Request) {
+	log.Println("New Connection")
+	ws, err := cs.upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ws.Close()
+	cs.clients[ws] = true
+
+	remoteid := ws.RemoteAddr().String()
+	remotename := "User" + remoteid
+
+	ticker := time.NewTicker(time.Duration(config.pingPeriod) * time.Second)
+	defer ticker.Stop()
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				// log.Println("Sending a ping to %s\n", remoteid)
+				ws.SetWriteDeadline(time.Now().Add(time.Duration(config.writeWait) * time.Second))
+				if err := ws.WriteMessage(websocket.PingMessage, nil); err != nil {
+					log.Println(err)
+					return
+				}
+			}
+		}
+	}()
+	ws.SetPongHandler(func(in string) error {
+		// log.Printf("got a pong from %s\n", remoteid)
+		msg := Message{
+			Name: remotename,
+			Id:   remoteid,
+			Pong: true,
+		}
+		b, berr := json.Marshal(msg)
+		if berr != nil {
+			log.Println(berr)
+		} else {
+			// log.Printf("Sending NATs Pong: %s\n", msg.Name)
+			cs.nc.Publish("meshchat.broadcast", b)
+		}
+
+		return nil
+	})
+
+	log.Println("Waiting for messages")
+	for {
+		var msg Message
+		err := ws.ReadJSON(&msg)
+		// log.Printf("New WS Message: %s - %s", msg.Name, msg.Message)
+		if err != nil {
+			log.Println("Error on ws: ", err)
+			log.Println(msg)
+			delete(cs.clients, ws)
+			break
+		}
+		//      if msg.Presence {
+		remoteid = msg.Id
+		remotename = msg.Name
+		//      }
+		if cs.natsconnected {
+			b, berr := json.Marshal(msg)
+			if berr != nil {
+				log.Println(berr)
+			} else {
+				log.Printf("Sending NATs Message: %s\n", msg.Message)
+				cs.nc.Publish("meshchat.broadcast", b)
+			}
+		}
+
+		//cs.broadcast <- msg
+	}
+}
+
+func (cs *ChatServer) sendtolocal(msg Message) {
+	cs.writemu.Lock()
+	defer cs.writemu.Unlock()
+	for client := range cs.clients {
+		client.SetWriteDeadline(time.Now().Add(time.Duration(config.writeWait) * time.Second))
+		err := client.WriteJSON(msg)
+		if err != nil {
+			log.Println("Error writing to client: ", err)
+			client.Close()
+			delete(cs.clients, client)
+		}
+
+	}
+
+}
+
+func (cs *ChatServer) handleNatsMsg(m *nats.Msg) {
+	var msg Message
+	err := json.Unmarshal(m.Data, &msg)
+	if err != nil {
+		log.Println(err)
+	} else {
+		// log.Printf("New NATs Message: %s - %s", msg.Name, msg.Message)
+		cs.sendtolocal(msg)
+	}
+}
--- main.go
+++ main.go
@@ -1,26 +1,35 @@
 package main
 
 import (
-	"encoding/json"
-	"flag"
 	"log"
 	"net/http"
 	"net/url"
+	"os"
 	"strconv"
-	"sync"
-	"time"
 
 	"github.com/gorilla/websocket"
 	"github.com/hashicorp/memberlist"
 	"github.com/nats-io/gnatsd/server"
 	"github.com/nats-io/go-nats"
+	"gopkg.in/alecthomas/kingpin.v2"
 )
 
-var writeWait = 3 * time.Second
-var pongWait = 3 * time.Second
-var pingPeriod = 2 * time.Second
-
 type peerlist []string
+
+type rootConfig struct {
+	writeWait       int
+	pongWait        int
+	pingPeriod      int
+	peers           peerlist
+	httpport        int
+	natsport        int
+	natsroutingport int
+	clusterport     int
+	debug           bool
+}
+
+var version string
+var config rootConfig
 
 func (i *peerlist) String() string {
 	r := ""
@@ -35,125 +44,6 @@ func (i *peerlist) Set(v string) error {
 	return nil
 }
 
-type ChatServer struct {
-	clients       map[*websocket.Conn]bool
-	broadcast     chan Message
-	upgrader      websocket.Upgrader
-	natsconnected bool
-	nc            *nats.Conn
-	ns            *server.Server
-	writemu       sync.Mutex
-}
-
-type Message struct {
-	Name     string `json:"name"`
-	Message  string `json:"message"`
-	Pong     bool   `json:"pong"`
-	Presence bool   `json:"presence"`
-	Id       string `json:"id"`
-}
-
-func (cs *ChatServer) handleChat(w http.ResponseWriter, r *http.Request) {
-	log.Println("New Connection")
-	ws, err := cs.upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer ws.Close()
-	cs.clients[ws] = true
-
-	remoteid := ws.RemoteAddr().String()
-	remotename := "User" + remoteid
-
-	ticker := time.NewTicker(pingPeriod)
-	defer ticker.Stop()
-	go func() {
-		for {
-			select {
-			case <-ticker.C:
-				// log.Println("Sending a ping to %s\n", remoteid)
-				ws.SetWriteDeadline(time.Now().Add(writeWait))
-				if err := ws.WriteMessage(websocket.PingMessage, nil); err != nil {
-					log.Println(err)
-					return
-				}
-			}
-		}
-	}()
-	ws.SetPongHandler(func(in string) error {
-		// log.Printf("got a pong from %s\n", remoteid)
-		msg := Message{
-			Name: remotename,
-			Id:   remoteid,
-			Pong: true,
-		}
-		b, berr := json.Marshal(msg)
-		if berr != nil {
-			log.Println(berr)
-		} else {
-			// log.Printf("Sending NATs Pong: %s\n", msg.Name)
-			cs.nc.Publish("meshchat.broadcast", b)
-		}
-
-		return nil
-	})
-
-	log.Println("Waiting for messages")
-	for {
-		var msg Message
-		err := ws.ReadJSON(&msg)
-		// log.Printf("New WS Message: %s - %s", msg.Name, msg.Message)
-		if err != nil {
-			log.Println("Error on ws: ", err)
-			log.Println(msg)
-			delete(cs.clients, ws)
-			break
-		}
-		//	if msg.Presence {
-		remoteid = msg.Id
-		remotename = msg.Name
-		//	}
-		if cs.natsconnected {
-			b, berr := json.Marshal(msg)
-			if berr != nil {
-				log.Println(berr)
-			} else {
-				log.Printf("Sending NATs Message: %s\n", msg.Message)
-				cs.nc.Publish("meshchat.broadcast", b)
-			}
-		}
-
-		//cs.broadcast <- msg
-	}
-}
-
-func (cs *ChatServer) sendtolocal(msg Message) {
-	cs.writemu.Lock()
-	defer cs.writemu.Unlock()
-	for client := range cs.clients {
-		client.SetWriteDeadline(time.Now().Add(writeWait))
-		err := client.WriteJSON(msg)
-		if err != nil {
-			log.Println("Error writing to client: ", err)
-			client.Close()
-			delete(cs.clients, client)
-		}
-
-	}
-
-}
-
-func (cs *ChatServer) handleNatsMsg(m *nats.Msg) {
-	var msg Message
-	err := json.Unmarshal(m.Data, &msg)
-	if err != nil {
-		log.Println(err)
-	} else {
-		// log.Printf("New NATs Message: %s - %s", msg.Name, msg.Message)
-		cs.sendtolocal(msg)
-	}
-}
-
 func RunServer(opts server.Options) *server.Server {
 	s, err := server.NewServer(&opts)
 	if err != nil || s == nil {
@@ -165,23 +55,28 @@ func RunServer(opts server.Options) *server.Server {
 }
 
 func main() {
-	// flags
-	httpport := flag.Int("httpport", 8080, "Port for HTTP/WS")
-	natsport := flag.Int("natsport", 4222, "Port for NATS messagebus internal server")
-	natsroutingport := flag.Int("natroutingport", 4001, "Port for NATS messagebus routing")
-	clusterport := flag.Int("clusterport", 4444, "Cluster Port")
-	debug := flag.Bool("debug", false, "Enable Debugging")
-	var peers peerlist
-	flag.Var(&peers, "peers", "peer list")
-	flag.Parse()
+	config = rootConfig{}
+	var peersraw []string
+	app := kingpin.New("meshchat", "Mesh Chat")
+	app.UsageTemplate(kingpin.CompactUsageTemplate)
+	app.Flag("writeWait", "NATS write wait").Default("3").IntVar(&config.writeWait)
+	app.Flag("pongWait", "NATS pong wait").Default("3").IntVar(&config.pongWait)
+	app.Flag("pingPeriod", "NATS ping period").Default("2").IntVar(&config.pingPeriod)
+	app.Flag("httpport", "HTTP Port").Default("8080").IntVar(&config.httpport)
+	app.Flag("natsport", "NATS Port").Default("4222").IntVar(&config.natsport)
+	app.Flag("natsroutingport", "NATS routing Port").Default("4001").IntVar(&config.natsroutingport)
+	app.Flag("clusterport", "Cluster port").Default("4444").IntVar(&config.clusterport)
+	app.Flag("debug", "Debug on").BoolVar(&config.debug)
+	app.Flag("peers", "Initial Peers List").StringsVar(&peersraw)
+	app.Version(version)
+	kingpin.MustParse(app.Parse(os.Args[1:]))
+	config.peers = peerlist(peersraw)
 
-	log.Println(*httpport)
-	log.Println(*natsport)
-	log.Println(*natsroutingport)
-	log.Println(*clusterport)
-	log.Println(peers)
+	if config.debug {
+		log.Printf("%+v", config)
+	}
 
-	// chat server internal stuff
+	// chat server internal
 	cs := &ChatServer{}
 	cs.clients = make(map[*websocket.Conn]bool)
 	cs.broadcast = make(chan Message)
@@ -191,8 +86,8 @@ func main() {
 
 	// cluster list
 	clusteropts := memberlist.DefaultWANConfig()
-	clusteropts.BindPort = *clusterport
-	clusteropts.AdvertisePort = *clusterport
+	clusteropts.BindPort = config.clusterport
+	clusteropts.AdvertisePort = config.clusterport
 	list, err := memberlist.Create(clusteropts)
 	if err != nil {
 		log.Println("Failed to start memberlist.. oh well", err.Error())
@@ -205,20 +100,20 @@ func main() {
 	// start Nats
 	opts := server.Options{
 		Host:           "127.0.0.1",
-		Port:           *natsport,
-		NoLog:          !*debug,
+		Port:           config.natsport,
+		NoLog:          !config.debug,
 		NoSigs:         true,
 		MaxControlLine: 2048,
 		Trace:          false,
-		Debug:          *debug,
+		Debug:          config.debug,
 		Cluster: server.ClusterOpts{
 			Host: "0.0.0.0",
-			Port: *natsroutingport,
+			Port: config.natsroutingport,
 		},
 	}
-	if len(peers) > 0 {
+	if len(config.peers) > 0 {
 		var routes []*url.URL
-		for _, i := range peers {
+		for _, i := range config.peers {
 			u, _ := url.Parse("nats-route://" + i)
 			routes = append(routes, u)
 		}
@@ -243,8 +138,8 @@ func main() {
 	fs := http.FileServer(assetFS())
 	http.Handle("/", fs)
 	http.HandleFunc("/ws", cs.handleChat)
-	log.Printf("Starting http on %d", *httpport)
-	err = http.ListenAndServe(":"+strconv.Itoa(*httpport), nil)
+	log.Printf("Starting http on %d", config.httpport)
+	err = http.ListenAndServe(":"+strconv.Itoa(config.httpport), nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
